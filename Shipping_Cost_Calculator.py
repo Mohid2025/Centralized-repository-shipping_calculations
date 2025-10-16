b65f43da0199226629abe77c58c0137d25c0768c
Mohid2025 <mohidiftikhar2025@example.com>
2025-10-16 11:56:25 +0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit b6f9016567600550c4a54d523dc2fedb5c1796fe.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
  # Here is a new update by Mohid2025
 
-# Here is another update by Mohid2025
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
